3904bf61205fe678f0a742463b236b595d9e9ec5
Naline Belo <62017582+Nahline@users.noreply.github.com>
2022-06-13 09:32:48 -0300

    Create CBBAtividadePratica3.java

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,12 @@
+public class CBBAtividadePratica3 {
+    public static void main(String args[]) {
+
+        String frase1;
+        String frase2;
+        frase1 = new String("testando igualdade!");
+        frase2 = new String("testando igualdade!");
+        System.out.println("frase 1:  " + frase1);
+        System.out.println("frase 2:  " + frase2);
+        System.out.println("A frase 1 e Frase 2 possuem o mesmo comteudo? R: " + (frase1.equals(frase2)));
+    }
+}
